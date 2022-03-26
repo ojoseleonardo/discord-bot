3c2e3886a081aa24c10be44ce9ad84fedd35c94d
José Leonardo <joseleonardosantoos@outlook.com>
2022-03-26 16:02:55 -0300

    Atualizando gitignore

--- src/br/com/andromeda/security/Token.java
+++ src/br/com/andromeda/security/Token.java
@@ -2,7 +2,7 @@ package br.com.andromeda.security;
 
 public class Token {
 
-    private  static final String  token = "OTU1ODM1NTExMzU5MDMzNDY0.YjndMA.DllllBb1ImF8cNZrHtyrL5kL9uI";
+    private  static final String  token = "OTU1ODM1NTExMzU5MDMzNDY0.YjndMA.MDNQiYGmDWpWeyNdSB2ho7AoA18";
 
     public static String getToken() {
         return token;
