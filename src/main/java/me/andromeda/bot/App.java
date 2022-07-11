a44d1beec1d0ba6edb8a8b24e4de6163c874593f
José Leonardo <joseleonardosantoos@outlook.com>
2022-07-11 18:09:50 -0300

    add aquivo de config, e comando de shutdown

--- src/main/java/me/andromeda/bot/App.java
+++ src/main/java/me/andromeda/bot/App.java
@@ -1,7 +1,6 @@
 package me.andromeda.bot;
 
 
-import me.andromeda.bot.token.Token;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
@@ -10,9 +9,10 @@ import javax.security.auth.login.LoginException;
 public class App {
 
     private App() throws LoginException {
-        new JDABuilder().setToken(Token.getToken())
+        new JDABuilder().setToken(Config.get("TOKEN"))
                 .setActivity(Activity.playing("Andrômeda"))
-                .addEventListeners(new Listener()).build();
+                .addEventListeners(new Listener())
+                .build();
     }
 
     public static void main(String[] args) throws LoginException {
--- src/main/java/me/andromeda/bot/Listener.java
+++ src/main/java/me/andromeda/bot/Listener.java
@@ -1,6 +1,8 @@
 package me.andromeda.bot;
 
+import me.duncte123.botcommons.BotCommons;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -14,4 +16,21 @@ public class Listener extends ListenerAdapter {
     public void onReady(@NotNull ReadyEvent event) {
         LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
     }
-}
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        String prefix = Config.get("prefix");
+        String raw = event.getMessage().getContentRaw();
+
+        if (raw.equalsIgnoreCase(prefix + "shutdown")
+                && event.getAuthor().getId().equals(Config.get("owner_id"))) {
+            LOGGER.info("Shutting down");
+            event.getJDA().shutdown();
+            BotCommons.shutdown(event.getJDA());
+        }
+        // fazer mensagem de texto incorporado aparecer aqui, e sumir dps de alguns segundos
+        else if (raw.equalsIgnoreCase(prefix + "shutdown")) {
+            LOGGER.info("User sem permissão");
+            }
+        }
+    }
